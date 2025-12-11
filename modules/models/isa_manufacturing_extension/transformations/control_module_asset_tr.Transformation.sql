28b775861b07b44a46828a527e707fa119ace08f
Sebastian Srikanth Kumar <sebastian.selvaraj@cognite.com>
2025-12-11 05:30:06 +0000

    fixed files, removed controlModule-Asset tr  and fixed slm data model

--- modules/models/isa_manufacturing_extension/transformations/control_module_asset_tr.Transformation.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  cast(key as string) AS externalId,
-  cast(control_module_name as string) AS name,
-  cast(description as string) as description,
-  CASE
-    WHEN equipment_module_externalId IS NULL OR equipment_module_externalId = '' THEN NULL
-    ELSE node_reference('sp_isa_instance_space', cast(equipment_module_externalId as string))
-  END AS parent
-FROM `ISA_Manufacturing`.`isa_control_module`
